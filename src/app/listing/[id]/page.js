ed5f6e270cc947ee69803e500909668f675d81dd
Dipjyoti Deka <dekadip94@gmail.com>
2026-04-03 12:14:37 +0530

    Fix deployment: remove static export and simplify dynamic routes

--- src/app/listing/[id]/page.js
+++ src/app/listing/[id]/page.js
@@ -1,11 +1,5 @@
 import ListingDetailClient from "./ListingDetailClient";
 
-export const dynamicParams = true;
-
-export async function generateStaticParams() {
-  return [];
-}
-
 export default function ListingPage() {
   return <ListingDetailClient />;
 }
